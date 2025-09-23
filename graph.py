738e1528fd04751bef24a18d013304717f15451c
ntfuko77 <ringochichidim@gmail.com>
2025-09-24 01:24:55 +0800

    graph class

--- graph.py
+++ graph.py
@@ -1,19 +1,19 @@
 class edge():
-    def __init__(self,data:'vertex',weight={}):
-        self.pointer=data
+    def __init__(self,source:'vertex',target:'vertex',weight={}):
+        self.source=source
+        self.target=target
         self.weight=weight
     def set_weight(self,data):
         self.weight.update(data)
 class vertex():
-    def __init__(self,data,edges=[]):
-        self.data=data
-        if False in [isinstance(i,edges) for i in edges]:
-            raise TypeError("vertex.edges only Vertex ")
-        self.edges=edges
-    def get_weight(self,name:str):
-        output=[i.weight for i in self.edges if (name in i.weight.keys())]
-        return output
+    def __init__(self,name):
+        self.name=name
+class graph():
+    def __init__(self):
+        self.vertexes={}
+        self.edges=[]
 
+    
 if __name__=='__main__':
     x=vertex('a')
     y=edge(x,{'b':1})
