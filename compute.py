c8672465ad4d67357ec7c4e6f220ef3b6ce17c81
ntfuko77 <ringochichidim@gmail.com>
2025-09-25 07:43:46 +0800

    release!

--- compute.py
+++ compute.py
@@ -10,34 +10,35 @@ class compute():
     def __init__(self):
         self.db=database(profile.db_name.value)
 
-    def search_path(self,source:str,target:str=None,ignore=profile.search_ignore.value):
+    def search_path(self,source:str,target:str,ignore=profile.search_ignore.value):
         check=self.db.classical_search_edge(source)
         if len(check)==0:
             raise ValueError(f'{source} not in vertex table')
         keys=check[1]
-        print(keys)
         output_weight=numpy.ones(len(keys)-1) #exclude target column
-        context=[i for i in check[0] if i[0] not in ignore]
-        output=context
-        stack=[]
-        if context==[]:
-            return []
-        sub=[numpy.array(i[2:]) for i in context]
-        number=0
-        while context!=[] or stack!=[]:
-            stack=stack+[i[0] for i in context]
-            next=stack.pop()
-            context=self.db.classical_search_edge(next)[0]
-            context=[i for i in context if i[0] not in ignore]
-            output=output+context
-        if target:
-            return [(x,y) for x,y in zip(keys[1:],output_weight)]
-        return output
+
+        def recursive_search(node:str,target,acc_weight,ignore:list):
+            if node==target:
+                return [(node,acc_weight)]
+            check=self.db.classical_search_edge(node)
+            if len(check[0])==0:
+                return [(node,acc_weight)] if node==target else []
+            paths=[]
+            for i in check[0]:
+                if i[0] in ignore:
+                    continue
+                weight=numpy.array(i[2:])
+                new_acc_weight=acc_weight*weight
+                # print(f'current node: {node} weight : {weight}, next node: {i[0]}, acc_weight: {new_acc_weight}')
+                paths+=recursive_search(i[0],target,new_acc_weight,ignore)
+            return paths
+        paths=recursive_search(source,target,output_weight,ignore)
+        return paths+[('summation',sum([i[1] for i in paths]))]
     
 
 if __name__=='__main__':
     c=compute()
-    o=c.search_path('cookie')
+    o=c.search_path('cookie','wheat')
     print(o)
 
     
